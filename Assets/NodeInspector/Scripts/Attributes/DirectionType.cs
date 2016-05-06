290690a992d3c91664260c7ae60762f4284f4940
nicloay <nicloay@gmail.com>
2016-05-06 10:31:56 +0300

    new attribute

---
+++ Assets/NodeInspector/Scripts/Attributes/DirectionType.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace NodeInspector {
+    public enum DirectionType {
+        Input,
+        Output
+    }
+}
---
+++ Assets/NodeInspector/Scripts/Attributes/ManyToManyAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace NodeInspector{    
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Class)]
+    public class OneToManyAttribute : Attribute {
+        public DirectionType Direction;
+        public string Key;
+        public OneToManyAttribute(DirectionType direction, string key){
+            Direction = direction;
+            Key = key;
+        }
+    }
+}
